e102c08d9feecea8b3d4b2a7a920d596d66217b1
MAINUL ISLAM <mainulislam6168@gmail.com>
2023-05-02 06:55:43 +0600

    multiple.html and double.js completed

---
+++ js/multiple.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-double').addEventListener('click', function(){
+
+    // step-1: get the input field value
+    const inputField = document.getElementById('input-number');
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value ='';
+    if(isNaN(inputFieldValue)){
+        alert('please Enter a Number');
+        return;
+    }
+
+    // step-2: get the output previous value 
+    const outputElement = document.getElementById('output');
+    const previousOutputResultString = outputElement.innerText;
+    const previousOutputResult = parseFloat(previousOutputResultString);
+    console.log(previousOutputResult)
+
+    // step-3: calculate the result
+    const newResult = inputFieldValue * 2 ;
+    outputElement.innerText = newResult;
+
+})
